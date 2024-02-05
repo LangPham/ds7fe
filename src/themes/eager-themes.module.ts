60bd79e2c39aa93ccab3e47a45ac1912603b7ba2
LangPham <phamxuanlang@gmail.com>
2024-02-05 14:07:37 +0700

    Edit home

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+// import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+import { EagerThemeModule as DSpaceEagerThemeModule } from './dgdu/eager-theme.module';
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
 
 /**
